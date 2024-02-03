3d1a0594c1b396e7203ffb8b6a8897fe101a8bb6
brunomry <brunomadozzo@gmail.com>
2024-02-03 16:38:18 -0300

    Agregue los componentes TituloPrincipal, TituloVeterinaria y Footer, y ubique el footer en la parte inferior de la pagina mediante estilos css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import TituloPrincipal from "./components/TituloPrincipal";
+import Footer from "./components/Footer";
+import "./App.css";
+import TituloVeterinaria from "./components/TituloVeterinaria";
+
 function App() {
   return (
     <>
-     
+      <Container className="my-2 mainPage">
+        <TituloPrincipal></TituloPrincipal>
+        <TituloVeterinaria/>
+      </Container>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TituloVeterinaria.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TituloVeterinaria = () => {
+  return (
+    <section className='sectionTitle py-2 my-4 rounded-2 mx-auto'>
+      <h2 className='text-center text-white display-5'>Administrar pacientes de veterinaria</h2>
+    </section>
+  );
+};
+
+export default TituloVeterinaria;
